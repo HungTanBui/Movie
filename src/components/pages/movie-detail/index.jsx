02fbf14dca1d88c5771fbf0129c4a79a311abdf5
HungTanBui <staboyvn12@gmail.com>
2025-08-15 19:39:54 +0700

    update movie detail

--- src/components/pages/movie-detail/index.jsx
+++ src/components/pages/movie-detail/index.jsx
@@ -1,78 +1,102 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import "./index.scss"
-import Tags from '../../tags';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./index.scss";
+import Tags from "../../tags";
+import { buildStyles, CircularProgressbar } from "react-circular-progressbar";
 
 function MovieDetail() {
+  // lấy cái slug ở trên url
 
-    // lấy cái slug ở trên url
+  const { movieId } = useParams();
 
-    const { movieId } = useParams();
+  const [movie, setMovie] = useState({});
 
-    const [movie, setMovie] = useState(null);
+  console.log(movieId);
 
-    console.log(movieId);
+  // thông qua API lấy movie detail
 
-    // thông qua API lấy movie detail
+  const fetchMovieDetail = async () => {
+    const respone = await axios.get(
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=a10ee5569194b352bcca20840b7f8a32`
+    );
+    setMovie(respone.data);
+    console.log(respone.data);
+  };
 
-    const fetchMovieDetail = async () => {
-        const respone = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=a10ee5569194b352bcca20840b7f8a32`);
-        setMovie(respone.data);
-        console.log(respone.data);
-    }
+  // chạy khi load page
 
-    // chạy khi load page
-
-    useEffect(() => {
-        fetchMovieDetail();
-    },[movieId]);
+  useEffect(() => {
+    fetchMovieDetail();
+  }, [movieId]);
 
   return (
     <div className="movie-detail">
-
       <div className="banner">
-
-        <img className="bg-blur"
-          src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`} alt="" />
+        <img
+          className="bg-blur"
+          src={`https://image.tmdb.org/t/p/original${movie?.backdrop_path}`}
+          alt=""
+        />
 
         <div className="contain">
-
           <div className="left">
-            <img classNam
-              src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`} alt="" />
+            <img
+              classNam
+              src={`https://image.tmdb.org/t/p/original${movie?.poster_path}`}
+              alt=""
+            />
           </div>
 
           <div className="right">
             <h1>{movie?.title}</h1>
-            <p>{movie?.tagline}</p>
-            <Tags>abc</Tags>
-          </div>
+            <p className="tagline">{movie?.tagline}</p>
+            {movie.genres?.map((genre) => (
+              <Tags>{genre.name}</Tags>
+            ))}
 
-          <div className="circleRating">
+            <div className="circleRating">
               <CircularProgressbar
-                value={movieDatail.vote_average}
+                value={movie.vote_average}
                 maxValue={10}
-                text={movieDatail.vote_average?.toFixed(1)}
+                text={movie.vote_average?.toFixed(1)}
                 styles={buildStyles({
                   pathColor:
-                    movieDatail.vote_average < 5
+                    movie.vote_average < 5
                       ? "red"
-                      : movieDatail.vote_average < 7
+                      : movie.vote_average < 7
                       ? "orange"
                       : "green",
                 })}
               />
             </div>
-        </div>
 
-        <div className="overlay"></div>
+            <h3>Overview</h3>
+            <p>{movie.overview}</p>
+            <div className="box">
+              <div className="info">
+                <span className="label">Status: </span>
+                <span className="value">{movie.status}</span>
+              </div>
+
+              <div className="info">
+                <span className="label">Release Date: </span>
+                <span className="value">{movie.release_date}</span>
+              </div>
+
+              <div className="info">
+                <span className="label">Runtime: </span>
+                <span className="value">{movie.runtime}</span>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default MovieDetail
+export default MovieDetail;
 
 // Home -> bộ phim bất kỳ  -> movie detail
 // movie detail
